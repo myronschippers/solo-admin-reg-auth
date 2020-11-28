85106200113b7251a4cf9cffeff770362a782e34
Myron <dubtwee@gmail.com>
2020-11-28 09:37:20 -0600

    creating DB setup for new registration process

--- database.sql
+++ database.sql
@@ -3,8 +3,24 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+CREATE TABLE "roles" (
+	"id" serial primary key,
+	"label" varchar(60) not null,
+	"access_level" int not null
+);
+
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "username" VARCHAR (80) UNIQUE,
+    "password" VARCHAR (1000),
+    "email" VARCHAR(80) UNIQUE NOT NULL,
+    "first_name" VARCHAR(100) NOT NULL,
+    "last_name" VARCHAR(100) NOT NULL,
+    "temp_reg_id" VARCHAR(500),
+    "role_id" INT REFERENCES "roles"
 );
+
+INSERT INTO "roles" ("label", "access_level")
+VALUES ('admin', 0),
+('manager', 3),
+('peon', 6);
