4205228019a66e42d495b225c537b5dfca930015
Myron <dubtwee@gmail.com>
2020-12-07 16:25:50 -0600

    production build for client

---
+++ build/precache-manifest.f8b55c69773294338759b1f1a9e811a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd5f52771491a841c666a7e9aebbd5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84884dace3364ad79758",
+    "url": "/static/css/main.2b01f072.chunk.css"
+  },
+  {
+    "revision": "5c2dcef410a60fe8f97f",
+    "url": "/static/js/2.8cb997b3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.8cb997b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84884dace3364ad79758",
+    "url": "/static/js/main.448f0856.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  }
+]);
